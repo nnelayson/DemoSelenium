328d4429e1e8a7424da40c497c189450fb4e0ece
nnelayson <jatayson@gmail.com>
2025-06-11 15:37:47 +0800

    Add links menu item and clickLinks method to ElementsPage

--- src/main/java/com/demoqa/pages/elements/ElementsPage.java
+++ src/main/java/com/demoqa/pages/elements/ElementsPage.java
@@ -7,10 +7,16 @@ import com.demoqa.pages.HomePage;
 public class ElementsPage extends HomePage{
 
      private By webTablesMenuItem = By.xpath("//li[@id='item-3']/span[text()='Web Tables']");
+     private By linksMenuItem = By.xpath("//li[@id='item-5']/span[text()='Links']");
 
      public WebTablesPage goToWebTables() {
          click(webTablesMenuItem);
          return new WebTablesPage();
+     }
+
+     public LinksPage clickLinks() {
+         click(linksMenuItem);
+         return new LinksPage();
      }
     
 }
